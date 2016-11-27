50ea9db21a143fc255cfbcfebf1e442f564aedf1
abhishek sugam <abhishek sugam>
2016-11-26 18:13:05 -0800

    importing required packages

---
+++ climate_police/tests/temperature.py
@@ -0,0 +1,12 @@
+import wget
+import os
+import zipfile
+import urllib3
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import plotly.offline as py
+import seaborn as sns
+import time
