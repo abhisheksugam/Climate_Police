0dfece20e4c801c43feacd67bfa7e67eb41729da
abhisheksugam <abhisheksugam@hotmail.com>
2016-11-28 02:23:00 -0800

    importing packages to run the analysis

---
+++ climate_police/tests/temperature.py
@@ -0,0 +1,17 @@
+"""temperature.py"""
+
+import wget
+import os
+import zipfile
+
+import urllib3
+import certifi
+import sys
+import glob
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import plotly.offline as py
+import seaborn as sns
