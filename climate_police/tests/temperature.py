ac831593181a284db5048c7dce61102636cf6938
abhisheksugam <abhisheksugam@hotmail.com>
2016-11-28 02:25:16 -0800

    creating function to download data after giving url & filename

--- climate_police/tests/temperature.py
+++ climate_police/tests/temperature.py
@@ -15,3 +15,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.offline as py
 import seaborn as sns
+
+def download_if_needed(URL, filename):
+    """
+    Download from URL to filename unless filename already exists
+
+    """
+
+    if os.path.exists(filename):
+        print('The File already exists !!')
+        return
+    else:
+        print('downloading', filename,'This may take several seconds... Please be patient :)')
+
+        # needed for https
+        http = urllib3.PoolManager(
+            cert_reqs='CERT_REQUIRED', # Force certificate check.
+            ca_certs=certifi.where(),  # Path to the Certifi bundle.
+            retries=3
+        )
+
+        try:
+            response = http.request('GET',URL)
+            with open(filename,'wb') as f:
+                f.write(response.data)
+            f.close()
+            response.release_conn()
+
+        except:
+            e = sys.exc_info()[0]
+            print('Error! Data not downloaded.',e)
